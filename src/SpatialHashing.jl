f53c7c78eb09e989294faca0a752dbb0dfd0c19b
Alexander Barth <barth.alexander@gmail.com>
2023-05-24 12:51:52 +0200

    update

--- src/SpatialHashing.jl
+++ src/SpatialHashing.jl
@@ -1,5 +1,78 @@
 module SpatialHashing
 
-# Write your package code here.
+lindex(ind,offsets) = sum((ind .- 1) .* offsets) + 1
+
+
+function hash(ind,max)
+    # https://matthias-research.github.io/pages/publications/tetraederCollision.pdf
+    large_primes = (73856093, 19349663, 83492791)[1:length(ind)]
+    return abs(reduce(xor,ind .* large_primes)) % max + 1
+end
+
+indices(x,h) = unsafe_trunc.(Int,x ./ h) .+ 1
+
+
+
+function spatial_hash!(particles,h,limits,table,num_particles)
+    table .= 0
+    num_particles .= 0
+    sz = unsafe_trunc.(Int,limits ./ h) .+ 1
+
+    # count particles
+
+    for i = 1:length(particles)
+        ind = indices(particles[i].x,h)
+        l = hash(ind,length(table))
+        table[l] += 1
+    end
+
+
+    # partial sums
+
+    for i = 2:length(table)
+        table[i] += table[i-1]
+    end
+
+
+    # fill-in
+
+    for i = 1:length(particles)
+        ind = indices(particles[i].x,h)
+        l = hash(ind,length(table))
+        num_particles[table[l]] = i
+        table[l] -= 1
+    end
+
+   # table .+= 1
+
+    return nothing
+end
+
+function spatial_hash(particles,h,limits)
+    sz = unsafe_trunc.(Int,limits ./ h) .+ 1
+    table = zeros(Int,prod(sz)+1)
+    num_particles = zeros(Int,length(particles))
+    @time spatial_hash!(particles,h,limits,table,num_particles)
+    return (; table, num_particles, h, sz)
+end
+
+function each_near(fun,x,search_range,spatial_index)
+    table,num_particles,h,sz = spatial_index
+    N = length(x)
+    ind = indices(x,h)
+
+
+    search = ntuple(N) do i
+        max(1,(ind[i]-search_range)):min(sz[i],(ind[i]+search_range))
+    end
+
+    for ind2 in CartesianIndices(search)
+        l = hash(Tuple(ind2),length(table))
+        for i = table[l]:table[l+1]
+            # must inline to avoid allocation
+            @inline fun(num_particles[i])
+        end
+    end
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,101 @@
+using SpatialHashing: spatial_hash!, spatial_hash, each_near
 using SpatialHashing
 using Test
+using StaticArrays
+using LinearAlgebra
+using StableRNGs
 
 @testset "SpatialHashing.jl" begin
-    # Write your tests here.
+    N = 2
+    rng = StableRNG(42)
+    nparticles = 1000
+    xpos = rand(N,nparticles)
+    particles = [(x=SVector(tuple(rand(rng,N)...)),) for i = 1:nparticles]
+
+    limits = (2,2)
+    h = 0.01
+
+    @show h,limits
+
+    sz = unsafe_trunc.(Int,limits ./ h) .+ 1
+    table = zeros(Int,prod(sz)+1)
+    num_particles = zeros(Int,length(particles))
+    limits = Tuple(limits)
+    @time spatial_hash!(particles,h,limits,table,num_particles)
+
+
+    #@code_warntype spatial_hash!(particles,h,limits,table,num_particles)
+
+
+
+    # #locations(i) = particles[i].x
+    # using StaticArrays
+    # import Base: size, getindex
+
+    # struct Location3{N,T,TC <: AbstractVector{Particle{N,T}}} <: AbstractVector{SVector{N,T}}
+    #     p::TC
+    # end
+    # @inline Base.size(l::Location3) = size(l.p)
+    # @inline Base.@propagate_inbounds Base.getindex(l::Location3,index) = l.p[index].x
+
+
+    # l = Location3(particles);
+
+    spatial_index = spatial_hash(particles,h,limits)
+
+    #@test table[end] == length(particles)
+
+
+    i = 1
+    pi = particles[i]
+    x = pi.x
+    search_range = 1
+    radius2 = h^2
+    offset = first(CartesianIndices(ntuple(i -> -search_range:search_range,N)))
+
+
+    function find_near!(spatial_index,particles,x,search_range,r2max,near_indices)
+        nfound = 0
+
+        @inline each_near(x,search_range,spatial_index) do j
+            pj = particles[j]
+	        rij = pj.x - x
+	        r2 = norm(rij)^2
+
+	        if r2 < r2max
+                nfound += 1
+                #            if nfound <= length(near_indices)
+                #nfound += 1
+                @inbounds near_indices[nfound] = j
+                #            end
+            end
+            #@inbounds near_indices[1] = j
+        end
+        return nfound
+    end
+
+    near_indices = zeros(Int,length(particles))
+    r2max = h^2;
+    nfound = find_near!(spatial_index,particles,x,search_range,r2max,near_indices)
+
+    near = near_indices[1:nfound]
+
+
+    near_ref = Int[]
+
+    for j = 1:length(particles)
+        pj = particles[j]
+	    rij = pj.x - pi.x
+	    r2 = norm(rij)^2
+
+	    if r2 < r2max
+            push!(near_ref,j)
+        end
+    end
+
+    @test Set(near_ref) == Set(near)
+
+
+
+
 end

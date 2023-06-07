1601150d1d4b8b0a2254bdd4450825b6fc5c9131
Alexander Barth <barth.alexander@gmail.com>
2023-06-07 21:42:18 +0200

    clean-up test

--- test/test_search.jl
+++ test/test_search.jl
@@ -24,34 +24,23 @@ end
 
 N = 2
 
-for npoints in (10,10000)
-   for h = (0.01, 0.4, 0.8)
+for npoints in (10,1000)
+    for h = (0.01, 0.4, 0.8)
         rng = StableRNG(42)
-        npoints = 10000
-        xpos = rand(N,npoints)
-        #points = [(x=SVector(tuple(rand(rng,N)...)),) for i = 1:npoints]
         points = [tuple(rand(rng,N)...) for i = 1:npoints]
 
         limits = (2,2)
 
         visited = zeros(Int,npoints)
-        sz = unsafe_trunc.(Int,limits ./ h) .+ 1
-        table = zeros(Int,prod(sz)+1)
-        num_points = zeros(Int,length(points))
-        limits = Tuple(limits)
-        @time spatial_hash!(points,h,limits,table,num_points)
 
         spatial_index = spatial_hash(points,h,limits)
-
-        #@test table[end] == length(points)
 
         search_range = 1
         near_indices = zeros(Int,length(points))
         r2max = h^2;
 
         for i = 1:length(points)
-            pi = points[i]
-            x = pi
+            x = points[i]
 
             nfound = find_near!(spatial_index,points,x,search_range,r2max,near_indices,visited)
 
@@ -62,7 +51,7 @@ for npoints in (10,10000)
 
                 for j = 1:length(points)
                     pj = points[j]
-	                rij = pj .- pi
+	                rij = pj .- x
 	                r2 = norm(rij)^2
 
 	                if r2 < r2max
